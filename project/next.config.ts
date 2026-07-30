1b97a232996bcab9ffbab9358c276e9db0324fc1
BianzCoSP <i_bianzjannkenrick.co@stratpoint.com>
2026-07-30 10:46:01 +0800

    chore(config): updated next.config.ts

--- project/next.config.ts
+++ project/next.config.ts
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
 	typescript: {
 		ignoreBuildErrors: true,
 	},
 	images: {
 		unoptimized: true,
 	},
+	cacheComponents: true,
 };
 
 export default nextConfig;
